458d492df76c811775b53e9e92d276099e9fd87d
oscarmiguel13 <carlo11171977@gmail.com>
2018-07-28 11:53:48 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,91 @@
+<!DOCTYPE html>
 <html>
-<head><title>Profile Page</title>
-<body>
-Hello World! Welcome
-<br><br>
-<?php echo '<p>Hello World</p>';?>
+<head>
+ <title>My Profile</title>
+</head>
+<style type="text/css">
+  body{
+      font-family: Arial, Helvetica, sans-serif;
+      margin-bottom: 20px;
+      padding-bottom: 20px;
+  }
+  table{
+      border: 1px solid #cccccc;
+      font-family: Arial, Helvetica, sans-serif;
+
+      }
+  td{
+      margin: 5px;
+      padding: 0 20px 0 20px;
+  }
+  .resume{
+      color: #2149d0;
+  }
+  .name{
+      color: #C70039;
+  }
+</style>
+<body style="width: 40%; margin: auto;">
+ <div>
+ <hr>
+ <h1 align="center" class="resume">RESUME</h1>
+ <hr>
+ <table align="center">
+  <tr>
+  <!--<table></table>    this is just a comment-->
+  <td colspan="40"><h4>Name: <u class="name">Carlo D. Petalver</u></h4></td>
+  <td rowspan="6" align="center"><a href=""><img src="user.png" width="100" height="100"></a></td>
+  </tr>
+  <tr>
+  <td>Address: Duol sa Talisay Intawn</td>
+  </tr>
+  <tr>
+  <td>Contact no: 12345667811</td>
+  </tr>
+  <tr>
+  <td>Birth date: February 30, 1989</td>
+  </tr>
+  </tr>
+  <td colspan="50"><hr></td><tr></tr>
+  <td><h2><b>Goals</b></h2></td>
+  <tr>
+   <td colspan="50"><p>Goals are the most important thing in a person’s life, without them your life would just be plain and boring not excitement at all. Without making goals in your life you would have nothing to look forward to, or even have anything to achieve.</p></td>
+  </tr>
+  <tr>
+   <td colspan="50"><hr></td><tr></tr>
+   <td><h2><b>Objectives</b></h2></td>
+  </tr>
+  <tr>
+   <td colspan="50"><p>As multifaceted as we human beings may be, once our existential needs are covered, we all finally strive for the same thing: We want to be perpetually happy and satisfied and we want to be able to live our lives without worries and cares. This search for harmony and inner peace, for self-realization, or however we like to describe this state, as our top objective in life, is practically the same in all the different methods of representing human needs. Not least the advertising industry also frequently attempts to make use of this striving for happiness and harmony.</p></td>
+  </tr>
+  <tr>
+   <td colspan="50"><hr></td><tr></tr>
+   <td><h3><b>About me</b></h3></td>
+  </tr>
+  <tr>
+   <td colspan="50"><h5>Madison is a director of brand marketing, with experience managing global teams and multi-million-dollar campaigns. Her background in brand strategy, visual design, and account management inform her mindful but competitive approach.</h5>
+    <h3><i>My hubbies</i></h3><ul>
+     <li><strong>Basketball</strong></li>
+     <li><em>Ping pong</em></li>
+     <li>Billiard</li>
+     <li>Boxing</li>
+    </ul>
+   </td>
+  </tr>
+  <tr>
+   <td>List of my fav foods:
+    <ol>
+     <li>Chicken Adobo Yummy</li>
+     <li>Pansit Canton Wow</li>
+     <li><h5>Palabok Alright</h5></li>
+    </ol>
+   </td>
+   <tr>
+   <ol>
+   </ol>
+   </tr>
+  </tr>
+ </table>
+ </div>
 </body>
 </html>
