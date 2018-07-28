826d9e5253bb9288f5e96675204a658a236a21fd
oscarmiguel13 <carlo11171977@gmail.com>
2018-07-28 14:08:41 +0800

    Create first.php

---
+++ first.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>Hello World</h1>
+<p>
+	<?php 
+		echo "Hi there.\n";
+		$answer = 6*7;
+		echo "The answer is $answer. What was the answer again?";
+	?>
+	<p>Yes another paragraph.</p>
+</p>
+</body>
+</html>
